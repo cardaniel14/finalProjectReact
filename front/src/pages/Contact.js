bf660587ea4df744d61c68e0ef1cd85825372878
Carlos Daniel Erazo <carlosderazo.07@gmail.com>
2023-10-08 20:49:45 +0100

    Style done, skills and hobbies are missing

--- front/src/pages/Contact.js
+++ front/src/pages/Contact.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "../../src/styles/components/pages/Contact.css";
 
 export default function Contact(props) {
   return (
-    <main className="holder contacto">
+    <main className="contacto">
       <div>
         <h2>Contacto rapido</h2>
         <form
@@ -14,20 +15,33 @@ export default function Contact(props) {
           <p>
             <label htmlFor="name">Nombre</label>
 
-            <input type="text" name="name" id="name" />
+            <input
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Your Name Here"
+            />
           </p>
           <p>
             <label htmlFor="email">Email</label>
-            <input type="email" name="email" id="email" />
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="E-mail Here"
+            />
           </p>
           <p>
             <label htmlFor="message">Mensaje</label>
+            <br />
             <textarea
               name="message"
               id="message"
               cols="30"
               rows="10"
+              placeholder="Your Message Here"
             ></textarea>
+            <br />
             <input type="submit" value="Enviar" />
           </p>
         </form>
--- front/src/pages/HomePage.js
+++ front/src/pages/HomePage.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "../styles/components/pages/HomePage.css";
 
 const HomePage = (props) => {
   return (
-    <main className="holder">
+    <main className="home">
       <div className="bienvenidos">
-        <h1>Carlos Daniel Erazo</h1>
+        <h1 className="mainTitle">Daniel Erazo</h1>
         <h2>Front-End Developer</h2>
       </div>
     </main>
